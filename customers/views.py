c985a3ac2abc60571c4edb56ed29f2eab16b4e6e
raveendranath <raveendranravi703@gmail.com>
2025-04-15 00:33:51 +0530

    order app completed

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .models import customer
 
 # Create your views here.
+
+def sign_out(request):
+    logout(request)
+    return redirect('home')
+
+def show_account(request):
+    context={}
+    if request.POST and 'register' in request.POST:
+        context['register']=True
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            email = request.POST.get('email')
+            address = request.POST.get('address')
+            phone = request.POST.get('phone')
+
+            # create user account
+            user = User.objects.create_user(
+                username=username,
+                password=password,
+                email=email
+            )
+
+            # create customer account
+            Customer = customer.objects.create(
+                user=user,
+                phone=phone,
+                address=address
+            )
+            # return redirect('home')
+            success_message="successfully registered"
+            messages.success(request,success_message)
+
+        except Exception as e:
+            # You should handle the exception here
+            error_message="Duplicate username or invalid input"
+            messages.error(request,error_message)
+            # You might want to add a message to the user
+            # or render the template with an error message
+    if request.POST and 'login' in request.POST:
+        context['register']=False
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user=authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'invalid user credentials')    
+        
+
+    return render(request, 'account.html',context)
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Order,orderedItem
+from products.models import product
 
 # Create your views here.
+def show_cart(request):
+    user=request.user
+    customer=user.customer_profile
+    cart_obj, created=Order.objects.get_or_create(
+           owner=customer,
+           order_status=Order.CART_STAGE
+         )
+    context={'cart':cart_obj}
+
+    return render(request,'cart.html',context)
+
+def add_to_cart(request):
+    if request.POST:
+        user = request.user
+        customer = user.customer_profile
+        quantity = int(request.POST.get('quantity')) 
+        product_id = request.POST.get('product_id')
+        
+        cart_obj, created = Order.objects.get_or_create(
+            owner=customer,
+            order_status=Order.CART_STAGE
+        )
+        
+        Product = product.objects.get(pk=product_id)
+        ordered_item, created = orderedItem.objects.get_or_create(
+            product=Product,
+            owner=cart_obj,    
+        )
+        
+        if created:
+            ordered_item.quantity = quantity  # Set initial quantity
+        else:
+            ordered_item.quantity += quantity  # Increment existing quantity
+            
+        ordered_item.save()  # Save the model instance, not the quantity field
+        return redirect('cart')
